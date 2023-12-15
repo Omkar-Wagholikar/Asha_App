b9dc57858363814c472ecc4e6ed53a72d085340d
Omkar-Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-12-15 12:04:00 +0530

    serving pdf's version 1 done

--- Asha_drf/Asha_Srv/api/views.py
+++ Asha_drf/Asha_Srv/api/views.py
@@ -11,6 +11,7 @@ from django.http import FileResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from pathlib import Path
 
 import os
 
@@ -40,14 +41,18 @@ def answer(request):
 
 class PdfFileView(APIView):
     def get(self, request, filename):
-        # short_report = open("F:/Asha/Asha_drf/Asha_Srv/pdf_files/book-no-1-page-2.pdf", 'rb')
-        # response = HttpResponse(FileWrapper(short_report), content_type='application/pdf')
-        # return FileResponse(filename="F:/Asha/Asha_drf/Asha_Srv/pdf_files/book-no-1-page-2.pdf")
-        print(os.getcwd())
-        import base64
-        short_report = open( os.getcwd() + f"/pdf_files/{filename}", 'rb')
-        report_encoded = base64.b64encode(short_report.read())
-        return Response({
-            'detail': filename, 
-            'filename': report_encoded
-            })
+        # print(os.getcwd())
+
+        # import base64
+        # short_report = open( os.getcwd() + f"/pdf_files/{filename}", 'rb')
+        # report_encoded = base64.b64encode(short_report.read())
+        # return Response({
+        #     'detail': filename, 
+        #     'filename': report_encoded
+        #     })
+        # pdf_file = get_object_or_404(Path, "F:/Asha/Asha_drf/Asha_Srv/pdf_files/book-no-1-page-2.pdf")
+        
+        response = FileResponse(open( os.getcwd() + f"/pdf_files/{filename}",'rb'))
+        response['Content-Disposition'] = f'inline; filename="{filename}"'
+        
+        return response
